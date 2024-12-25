7a5343500723ca07cce564a0654debdf9472e3f5
GiaTuan <giatuan@wptangtoc.com>
2024-12-26 01:22:24 +0700

    cai tien cai ssl website chuyen huong

--- tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
+++ tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
@@ -9,10 +9,14 @@ fi
 
 if [[ $NAME = '' ]];then
 pwd=$(pwd)
-NAME=$(echo $pwd | cut -f1-6 -d '/')
+NAME=$(echo $pwd | cut -f1-6 -d '/'|cut -f5 -d '/')
 fi
 
 
+if [[ $NAME = '' ]];then
+	echo "Không xác định được tên website cần thực thi"
+	exit
+fi
 if [[ -f /usr/local/lsws/$NAME/html/wp-content/plugins/wptangtoc/class/PreloadAllPHP.php ]];then
 	. /etc/wptt/vhost/.$NAME.conf
 	if [[ $phien_ban_php_domain = '' ]];then
@@ -26,4 +30,5 @@ chown "$User_name_vhost":"$User_name_vhost" /usr/local/lsws/$NAME/html/wp-conten
       echo
     )
 curl -s https://$NAME/?wptangtoc_cache=$random 
+echo "Triển khai Preload Cache PHP"
 fi
