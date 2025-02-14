f9fbe74497875e82b3d0d57a16a6d0171608c0bc
GiaTuan <giatuan@wptangtoc.com>
2025-02-14 16:26:52 +0700

    nftables update

--- tool-wptangtoc-ols/bao-mat/nftables/nftables-ddos-bao-mat.sh
+++ tool-wptangtoc-ols/bao-mat/nftables/nftables-ddos-bao-mat.sh
@@ -62,7 +62,7 @@ cat <(crontab -l) <(echo "*/1 * * * * echo '' > /usr/local/lsws/$NAME/logs/acces
 #file config /etc/sysconfig/nftables.conf
 
 
-if [[ $(cat /etc/sysconfig/nftables.conf | grep 'ipvietnam') = '' ]];then
+if [[ $(cat $path_nftables_config | grep 'ipvietnam') = '' ]];then
 
 	if $(cat /etc/*release | grep -q "ubuntu") ; then
 		cp -f /etc/wptt/bao-mat/nftables/nftables.conf /etc
@@ -78,15 +78,11 @@ if [[ $port_checkssh = '' ]];then
 port_checkssh=22
 fi
 
-port_checkssh=$(cat /etc/ssh/sshd_config | grep "Port " | grep -o '[0-9]\+$')
-if [[ $port_checkssh = '' ]];then
-port_checkssh=22
-fi
 sed -i "/chain input /a\ \ tcp dport $port_checkssh accept #port ssh" $path_nftables_config
 systemctl restart nftables
 
 path_webgui="/usr/local/lsws/conf/disablewebconsole"
-if [[ -f $path_webgui ]]; then
+if [[ ! -f $path_webgui ]]; then
 port_webgui_openlitespeed=$(cat /usr/local/lsws/admin/conf/admin_config.conf | grep "address" | cut -f2 -d":")
 sed -i "/chain input /a\ \ tcp dport $port_webgui_openlitespeed accept #port webguiadmin" $path_nftables_config
 systemctl restart nftables
