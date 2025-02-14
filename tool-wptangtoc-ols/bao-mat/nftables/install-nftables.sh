ee737d0cae8254f73101f8d642cfddb1319bebf2
GiaTuan <giatuan@wptangtoc.com>
2025-02-14 16:26:52 +0700

    nftables update

--- tool-wptangtoc-ols/bao-mat/nftables/install-nftables.sh
+++ tool-wptangtoc-ols/bao-mat/nftables/install-nftables.sh
@@ -14,13 +14,13 @@ systemctl unmask nftables
 systemctl start nftables
 systemctl enable nftables
 
-cat /etc/wptt/bao-mat/nftables/nftables-co-ban.conf > /etc/sysconfig/nftables.conf
-
 if $(cat /etc/*release | grep -q "Ubuntu") ; then
 		path_nftables_config="/etc/nftables.conf"
 	else
 		path_nftables_config="/etc/sysconfig/nftables.conf"
 fi
+cat /etc/wptt/bao-mat/nftables/nftables-co-ban.conf > $path_nftables_config
+
 
 port_checkssh=$(cat /etc/ssh/sshd_config | grep "Port " | grep -o '[0-9]\+$')
 if [[ $port_checkssh = '' ]];then
@@ -34,7 +34,7 @@ systemctl restart nftables
 
 
 path_webgui="/usr/local/lsws/conf/disablewebconsole"
-if [[ -f $path_webgui ]]; then
+if [[ ! -f $path_webgui ]]; then
 port_webgui_openlitespeed=$(cat /usr/local/lsws/admin/conf/admin_config.conf | grep "address" | cut -f2 -d":")
 sed -i "/chain input /a\ \ tcp dport $port_webgui_openlitespeed accept #port webguiadmin" $path_nftables_config
 systemctl restart nftables
