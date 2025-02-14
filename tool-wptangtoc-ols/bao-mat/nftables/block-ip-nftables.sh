877ac7a1ad6acb7292b9e0ff264b5c45ade4936c
GiaTuan <giatuan@wptangtoc.com>
2025-02-14 15:17:12 +0700

    beta nftables thay the firewalld

---
+++ tool-wptangtoc-ols/bao-mat/nftables/block-ip-nftables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ip="$1"
+
+if [[ $ip = '' ]];then
+	read -p "Nhập địa chỉ ip bạn muốn block" ip
+fi
+
+if [[ $(echo $ip |grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)') = '' ]];then
+	echo "nhập ip không đúng định dạng"
+exit
+fi
+
+nft add element ip blackblock blackaction "{$ip}"
+
