94c85691859231dc84d4c70df9bf6a40fdea8be3
GiaTuan <giatuan@wptangtoc.com>
2025-06-08 16:08:23 +0700

    cải thiện chuyển nhánh repo mariadb

--- tool-wptangtoc-ols/bao-mat/nftables/nftables-ddos-bao-mat-rewrite-quoc-te.sh
+++ tool-wptangtoc-ols/bao-mat/nftables/nftables-ddos-bao-mat-rewrite-quoc-te.sh
@@ -2,8 +2,8 @@
 
 nftables_service=$(systemctl status nftables.service 2>/dev/null | grep 'Active' | cut -f2 -d':' | xargs | cut -f1 -d' ' | xargs)
 if [[ "$nftables_service" != "active" ]]; then
-echo "nftables chưa được cài đặt vui lòng cài đặt nftables"
-exit
+  echo "nftables chưa được cài đặt vui lòng cài đặt nftables"
+  exit
 fi
 
 . /etc/wptt/tenmien
@@ -14,18 +14,17 @@ echo ""
 lua_chon_NAME
 . /etc/wptt/echo-color
 if [[ "$NAME" = "0" || "$NAME" = "" ]]; then
-    . /etc/wptt/wptt-wordpress-main 1
+  . /etc/wptt/wptt-wordpress-main 1
 fi
 
 pathcheck="/etc/wptt/vhost/.$NAME.conf"
 if [[ ! -f "$pathcheck" ]]; then
-    clear
-	echoDo "Tên miền không tồn tại trên hệ thống này"
-    sleep 3
-    . /etc/wptt/wptt-wordpress-main 1
-    exit
+  clear
+  echoDo "Tên miền không tồn tại trên hệ thống này"
+  sleep 3
+  . /etc/wptt/wptt-wordpress-main 1
+  exit
 fi
-
 
 dnf install python3 jq -y
 
@@ -37,10 +36,10 @@ systemctl mask fail2ban
 mkdir -p /usr/local/lsws/$NAME/bao-mat
 cp -f /etc/wptt/bao-mat/nftables/anti2.py /usr/local/lsws/$NAME/bao-mat/anti.py
 
-if $(cat /etc/*release | grep -q "AlmaLinux\|Rocky\|CentOS") ; then
-systemctl restart crond
+if $(cat /etc/*release | grep -q "AlmaLinux\|Rocky\|CentOS"); then
+  systemctl restart crond
 else
-systemctl restart cron
+  systemctl restart cron
 fi
 
 chmod +x /usr/local/lsws/$NAME/bao-mat/anti.py
@@ -51,7 +50,6 @@ chmod +x /usr/local/lsws/$NAME/bao-mat/anti.py
 cat <(crontab -l) | sed "/bao-mat/d" | crontab -
 cat <(crontab -l) | sed "/truncate/d" | crontab -
 
-
 cat <(crontab -l) <(echo "* * * * * /usr/bin/python3 /usr/local/lsws/$NAME/bao-mat/anti.py >/dev/null 2>&1") | crontab -
 cat <(crontab -l) <(echo "* * * * * sleep 10; /usr/bin/python3 /usr/local/lsws/$NAME/bao-mat/anti.py >/dev/null 2>&1") | crontab -
 cat <(crontab -l) <(echo "* * * * * sleep 20; /usr/bin/python3 /usr/local/lsws/$NAME/bao-mat/anti.py >/dev/null 2>&1") | crontab -
@@ -60,46 +58,41 @@ cat <(crontab -l) <(echo "* * * * * sleep 40; /usr/bin/python3 /usr/local/lsws/$
 cat <(crontab -l) <(echo "* * * * * sleep 50; /usr/bin/python3 /usr/local/lsws/$NAME/bao-mat/anti.py >/dev/null 2>&1") | crontab -
 cat <(crontab -l) <(echo "*/2 * * * * truncate -s 0 /usr/local/lsws/logs/error.log") | crontab -
 
-
-	if $(cat /etc/*release | grep -q "Ubuntu") ; then
-		path_nftables_config="/etc/nftables.conf"
-	else
-		path_nftables_config="/etc/sysconfig/nftables.conf"
-	fi
+if $(cat /etc/*release | grep -q "Ubuntu"); then
+  path_nftables_config="/etc/nftables.conf"
+else
+  path_nftables_config="/etc/sysconfig/nftables.conf"
+fi
 
 #file config /etc/sysconfig/nftables.conf
 
+if [[ $(cat $path_nftables_config | grep 'ipvietnam') = '' ]]; then
 
-if [[ $(cat $path_nftables_config | grep 'ipvietnam') = '' ]];then
+  if $(cat /etc/*release | grep -q "ubuntu"); then
+    cp -f /etc/wptt/bao-mat/nftables/nftables-khong-block-quoc-gia.conf $path_nftables_config
+  else
+    cp -f /etc/wptt/bao-mat/nftables/nftables-khong-block-quoc-gia.conf $path_nftables_config
+  fi
 
-	if $(cat /etc/*release | grep -q "ubuntu") ; then
-		cp -f /etc/wptt/bao-mat/nftables/nftables-khong-block-quoc-gia.conf $path_nftables_config
-	else
-		cp -f /etc/wptt/bao-mat/nftables/nftables-khong-block-quoc-gia.conf $path_nftables_config
-	fi
+  chmod 600 $path_nftables_config
 
-	chmod 600 $path_nftables_config
+  #mở port ssh
+  port_checkssh=$(cat /etc/ssh/sshd_config | grep "Port " | grep -o '[0-9]\+$')
+  if [[ $port_checkssh = '' ]]; then
+    port_checkssh=22
+  fi
 
-#mở port ssh
-port_checkssh=$(cat /etc/ssh/sshd_config | grep "Port " | grep -o '[0-9]\+$')
-if [[ $port_checkssh = '' ]];then
-port_checkssh=22
-fi
+  sed -i "/chain input /a\ \ tcp dport $port_checkssh accept #port ssh" $path_nftables_config
+  systemctl restart nftables
 
-sed -i "/chain input /a\ \ tcp dport $port_checkssh accept #port ssh" $path_nftables_config
-systemctl restart nftables
-
-path_webgui="/usr/local/lsws/conf/disablewebconsole"
-if [[ ! -f $path_webgui ]]; then
-port_webgui_openlitespeed=$(cat /usr/local/lsws/admin/conf/admin_config.conf | grep "address" | grep -o '[0-9]\+$')
-sed -i "/chain input /a\ \ tcp dport $port_webgui_openlitespeed accept #port webguiadmin" $path_nftables_config
-systemctl restart nftables
-fi
-
+  path_webgui="/usr/local/lsws/conf/disablewebconsole"
+  if [[ ! -f $path_webgui ]]; then
+    port_webgui_openlitespeed=$(cat /usr/local/lsws/admin/conf/admin_config.conf | grep "address" | grep -o '[0-9]\+$')
+    sed -i "/chain input /a\ \ tcp dport $port_webgui_openlitespeed accept #port webguiadmin" $path_nftables_config
+    systemctl restart nftables
+  fi
 
 fi
-
-
 
 #google=$(curl -s  https://developers.google.com/static/search/apis/ipranges/googlebot.json | jq '.prefixes| .[]|.ipv4Prefix' | sed '/null/d'|  sed 's/"//g' | sed 's/ /\n/g'| sed '/^$/d')
 #google=$(echo $google | sed 's/ /, /g')
@@ -119,12 +112,7 @@ fi
 
 #nft list ruleset > $path_nftables_config
 
-ip=$(curl -skf --connect-timeout 5 --max-time 10 https://ipv4.icanhazip.com |grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' || curl -skf --connect-timeout 5 --max-time 10 https://checkip.amazonaws.com| grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
-
-. /etc/wptt/bao-mat/nftables/bypass-ip.sh $ip
-
 . /etc/wptt/logs/error-chuyen-warn-log-server
-
 
 systemctl restart nftables
 
