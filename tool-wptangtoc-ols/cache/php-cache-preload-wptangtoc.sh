53561a81bd01985887216bf650780c6cbb936aac
GiaTuan <giatuan@wptangtoc.com>
2025-07-14 23:01:58 +0700

    xoá esmtp thưc viện được tich hợp sẵn trong fail2ban, cải thiện hiệu suất

--- tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
+++ tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
@@ -22,6 +22,11 @@ if [[ -f /usr/local/lsws/$NAME/html/wp-content/plugins/wptangtoc/class/PreloadAl
   if [[ -z $phien_ban_php_domain ]]; then
     phien_ban_php_domain=$(php -v | grep cli | grep -oP 'PHP \K[0-9]+\.[0-9]+')
   fi
+
+  if [[ $(echo $phien_ban_php_domain | grep 'lsphp') ]]; then #tương thích ngươcj với thời xưa đặt phiên bản php là lsphp
+    phien_ban_php_domain=$(echo $phien_ban_php_domain | sed 's/lsphp//g')
+  fi
+
   phien_ban_php_domain=${phien_ban_php_domain//[-._]/}
   /usr/local/lsws/lsphp${phien_ban_php_domain}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
   chown "$User_name_vhost":"$User_name_vhost" /usr/local/lsws/$NAME/html/wp-content/preload-wptangtoc-url.txt
