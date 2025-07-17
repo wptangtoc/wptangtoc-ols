9bdbb22442192b2b5643c7b8c1dc12b3a257543d
GiaTuan <giatuan@wptangtoc.com>
2025-07-17 22:45:58 +0700

    cải tiến chuyển hướng tránh xung đột với lscache

--- tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
+++ tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
@@ -17,17 +17,7 @@ if [[ $NAME = '' ]]; then
   exit
 fi
 if [[ -f /usr/local/lsws/$NAME/html/wp-content/plugins/wptangtoc/class/PreloadAllPHP.php ]]; then
-  unset phien_ban_php_domain
-  . /etc/wptt/vhost/.$NAME.conf
-  if [[ -z $phien_ban_php_domain ]]; then
-    phien_ban_php_domain=$(php -v | grep cli | grep -oP 'PHP \K[0-9]+\.[0-9]+')
-  fi
-
-  if [[ $(echo $phien_ban_php_domain | grep '^lsphp') ]]; then #tương thích ngươcj với thời xưa đặt phiên bản php là lsphp
-    phien_ban_php_domain=$(echo $phien_ban_php_domain | sed 's/^lsphp//g')
-  fi
-
-  phien_ban_php_domain=${phien_ban_php_domain//[-._]/}
+  . /etc/wptt/php/php-cli-domain-config $NAME
   /usr/local/lsws/lsphp${phien_ban_php_domain}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
   chown "$User_name_vhost":"$User_name_vhost" /usr/local/lsws/$NAME/html/wp-content/preload-wptangtoc-url.txt
   random=$(shuf -i 0-9 -n 10 -r | tr -d '\n')
