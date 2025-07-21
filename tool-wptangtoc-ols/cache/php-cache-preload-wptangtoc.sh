9bce5734b9f9f030d404dd736708420b6e83342d
GiaTuan <giatuan@wptangtoc.com>
2025-07-21 18:01:34 +0700

    cải tiến phân quyền

--- tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
+++ tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
@@ -18,7 +18,9 @@ if [[ $NAME = '' ]]; then
 fi
 if [[ -f /usr/local/lsws/$NAME/html/wp-content/plugins/wptangtoc/class/PreloadAllPHP.php ]]; then
   . /etc/wptt/php/php-cli-domain-config $NAME
-  /usr/local/lsws/lsphp${phien_ban_php_domain}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
+  # /usr/local/lsws/lsphp${phien_ban_php_domain}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
+  /usr/local/lsws/lsphp${phien_ban_php_domain_thuc_thi}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
+
   chown "$User_name_vhost":"$User_name_vhost" /usr/local/lsws/$NAME/html/wp-content/preload-wptangtoc-url.txt
   random=$(shuf -i 0-9 -n 10 -r | tr -d '\n')
   curl -s https://$NAME/?wptangtoc_cache=${random} -A "WPTangToc OLS preload cache"
