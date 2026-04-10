4492b17048a30b24931d5ff673e22069eec806bd
GiaTuan <giatuan@wptangtoc.com>
2026-04-10 10:50:47 +0700

    cải tiến preload cache

--- tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
+++ tool-wptangtoc-ols/cache/php-cache-preload-wptangtoc.sh
@@ -12,17 +12,23 @@ if [[ $NAME = '' ]]; then
   NAME=$(echo $pwd | cut -f1-6 -d '/' | cut -f5 -d '/')
 fi
 
-if [[ $NAME = '' ]]; then
+if [[ -z $NAME ]]; then
   echo "Không xác định được tên website cần thực thi"
-  exit
+  return 2>/dev/null || exit
 fi
+
 if [[ -f /usr/local/lsws/$NAME/html/wp-content/plugins/wptangtoc/class/PreloadAllPHP.php ]]; then
+  PATH='/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin' #bien moi truong full day du
+  . /etc/wptt/vhost/.$NAME.conf
   . /etc/wptt/php/php-cli-domain-config $NAME
   # /usr/local/lsws/lsphp${phien_ban_php_domain}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
-  /usr/local/lsws/lsphp${phien_ban_php_domain_thuc_thi}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
+  echo "Preload Cache PHP html cache website $NAME: $(date '+%d-%m-%Y %H:%M')" >>/var/log/wptangtoc-ols.log
 
-  chown "$User_name_vhost":"$User_name_vhost" /usr/local/lsws/$NAME/html/wp-content/preload-wptangtoc-url.txt
-  random=$(shuf -i 0-9 -n 10 -r | tr -d '\n')
-  curl -s https://$NAME/?wptangtoc_cache=${random} -A "WPTangToc OLS preload cache"
+  sudo -u $User_name_vhost /usr/local/lsws/lsphp${phien_ban_php_domain_thuc_thi}/bin/php /usr/local/bin/wp eval 'WPTangToc\PreloadAllPHP::preload_cache();' --allow-root --path=/usr/local/lsws/$NAME/html >/dev/null 2>&1
+
+  # chown "$User_name_vhost":"$User_name_vhost" /usr/local/lsws/$NAME/html/wp-content/preload-wptangtoc-url.txt
+  # random=$(shuf -i 0-9 -n 10 -r | tr -d '\n')
+  random=$(tr -dc '0-9' </dev/urandom | head -c 10)
+  curl -s https://"$NAME"/?wptangtoc_cache=${random} -A "WPTangToc OLS preload cache"
   echo "Triển khai Preload Cache PHP"
 fi
